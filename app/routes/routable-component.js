634b1fa8bebacca6b1cbcb67d04d21064a5112b9
Michael Smith <michaelanthonysmith00@gmail.com>
2016-04-11 16:25:03 -0700

    add overlays to display a little house.

--- app/routes/routable-component.js
+++ app/routes/routable-component.js
@@ -5,7 +5,49 @@ export default Ember.Route.extend({
    controller.setProperties({
      lat: 45.5231,
      lng: -122.6765,
-     zoom: 12
+     zoom: 15,
+     overlays: Ember.A([
+       {
+         id: '1',         // Recommended
+         lat: 45.5231,  // Required
+         lng: -122.6765,        // Required
+         content: '<h3 class="glyphicon glyphicon-home"></h3>',
+         layer: 'floatPane',       // Id of any of the layers defined in google.maps.MapPanes
+         verticalAlign: 'top',      // middle | bottom
+         horizontalAlign: 'center', // left | right
+         horizontalOffset: 50,      // in pixels
+         verticalOffset: 50,        // in pixels
+         click: function(event, overlay) {
+           alert("hey!");
+         },
+         dblclick: function(event, overlay) {},
+         mouseup: function(event, overlay) {},
+         mousedown: function(event, overlay) {},
+         mouseover: function(event, overlay) {},
+         mousemove: function(event, overlay) {},
+         mouseout: function(event, overlay) {}
+       },
+       {
+         id: '2',         // Recommended
+         lat: 45.526002,  // Required
+         lng: -122.681050,        // Required
+         content: '<h3 class="glyphicon glyphicon-home"></h3>',
+         layer: 'floatPane',       // Id of any of the layers defined in google.maps.MapPanes
+         verticalAlign: 'top',      // middle | bottom
+         horizontalAlign: 'center', // left | right
+         horizontalOffset: 50,      // in pixels
+         verticalOffset: 50,        // in pixels
+         click: function(event, overlay) {
+           alert("yo!");
+         },
+         dblclick: function(event, overlay) {},
+         mouseup: function(event, overlay) {},
+         mousedown: function(event, overlay) {},
+         mouseover: function(event, overlay) {},
+         mousemove: function(event, overlay) {},
+         mouseout: function(event, overlay) {}
+       }
+     ])
    });
  }
 });
