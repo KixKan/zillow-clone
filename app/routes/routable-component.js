21aa69df038ebcffc6248cdd1df53759d7b33dea
nathan otto <ottoetc@gmail.com>
2016-04-12 16:14:02 -0700

    add a another house

--- app/routes/routable-component.js
+++ app/routes/routable-component.js
@@ -9,7 +9,7 @@ export default Ember.Route.extend({
      zoom: 15,
      overlays: Ember.A([
        {
-         id: '1',         // Recommended
+         id: '0',         // Recommended
          lat: 45.5231,  // Required
          lng: -122.6765,        // Required
          content: '<a href="http://localhost:4200/detail/0"><h4 class="glyphicon glyphicon-home"></h4></a>',
@@ -26,7 +26,7 @@ export default Ember.Route.extend({
          mouseout: function(event, overlay) {}
        },
        {
-         id: '2',         // Recommended
+         id: '1',         // Recommended
          lat: 45.526002,  // Required
          lng: -122.681050,        // Required
          content: '<a href="http://localhost:4200/detail/1"><h4 class="glyphicon glyphicon-home"></h4></a>',
@@ -43,10 +43,27 @@ export default Ember.Route.extend({
          mouseout: function(event, overlay) {}
        },
        {
-         id: '3',         // Recommended
+         id: '2',         // Recommended
          lat: 45.526020,  // Required
          lng: -122.690931,        // Required
          content: '<a href="http://localhost:4200/detail/2"><h4 class="glyphicon glyphicon-home"></h4></a>',
+         layer: 'floatPane',       // Id of any of the layers defined in google.maps.MapPanes
+         verticalAlign: 'top',      // middle | bottom
+         horizontalAlign: 'center', // left | right
+         click: function(event, overlay) {
+         },
+         dblclick: function(event, overlay) {},
+         mouseup: function(event, overlay) {},
+         mousedown: function(event, overlay) {},
+         mouseover: function(event, overlay) {},
+         mousemove: function(event, overlay) {},
+         mouseout: function(event, overlay) {}
+       },
+       {
+         id: '3',         // Recommended
+         lat: 45.514703,  // Required
+         lng: -122.685771,        // Required
+         content: '<a href="http://localhost:4200/detail/3"><h4 class="glyphicon glyphicon-home"></h4></a>',
          layer: 'floatPane',       // Id of any of the layers defined in google.maps.MapPanes
          verticalAlign: 'top',      // middle | bottom
          horizontalAlign: 'center', // left | right
