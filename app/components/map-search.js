48a6079859f740333c1cc6632fc98c7cb2e95c3a
Michael Smith <michaelanthonysmith00@gmail.com>
2016-04-11 17:04:45 -0700

    add code to implemenet geo code search

---
+++ app/components/map-search.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  gMap: Ember.inject.service(),
+
+  actions: {
+    standardGeocode(address) {
+      this.get('gMap')
+        .geocode({address})
+        .then((geocodes) => {
+          // Get first geocode latitude
+          console.log(geocodes[0].geometry.location.lat());
+        })
+        .catch((err) => console.error(err));
+    }
+  }
+
+});
--- app/routes/routable-component.js
+++ app/routes/routable-component.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  
+
   setupController: function(controller) {
    controller.setProperties({
      lat: 45.5231,
