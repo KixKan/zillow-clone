aebb6ac868960a66774e90af6c5e3770ae4b758a
Michael Smith <michaelanthonysmith00@gmail.com>
2016-04-13 11:42:28 -0700

    add helper for new paragraph

---
+++ tests/unit/helpers/text-format-test.js
@@ -0,0 +1,10 @@
+import { textFormat } from 'zillow-clone/helpers/text-format';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | text format');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = textFormat([42]);
+  assert.ok(result);
+});
